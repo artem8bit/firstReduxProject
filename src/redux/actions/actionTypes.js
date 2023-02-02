0e7c3face8c79c65de534caac39dedb8accbdc27
artem8bit <bodunov232@gmail.com>
2023-02-02 10:01:59 +0300

    del_count

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,9 +1,7 @@
 //text
 export const SET_TEXT = 'SET_TEXT'
 export const SET_EDIT_TEXT = 'SET_EDIT_TEXT'
-//count
-export const SET_DEC = 'SET_DEC'
-export const SET_INC = 'SET_INC'
+
 //todo
 export const SET_TODO = "SET_TODO"
 export const DEL_TODO = "DEL_TODO"
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,13 +2,11 @@
 
 import { combineReducers } from "redux";
 import textReducer from './textReducer'
-import countReducer from "./countReducer";
 import todosReducer from "./todosReducer";
 import editTextReducer from './editTextReducer'
 
 export default combineReducers({
   text: textReducer,
-  count: countReducer,
   todos: todosReducer,
   editText: editTextReducer,
 })
